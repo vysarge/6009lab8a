3375cb16d150d32ffb645d4c40c16a8c7d1935a0
vysarge <vysarge@gmail.com>
2018-04-19 23:41:28 -0400

    result_and_env done, section 7.4.2

--- lab.py
+++ lab.py
@@ -120,14 +120,53 @@ def parse(tokens):
         raise SyntaxError('Unopened close parenthesis or uncaught error type in parser')
     return parsed_expression
 
+class CarlaeEnvironment:
+    def __init__(self, parent=None):
+        assert parent==None or isinstance(parent, CarlaeEnvironment), \
+            'Parent of a CarlaeEnvironment must also be a CarlaeEnvironment'
+        self.parent = parent
+        self.defs = {}
+
+    def __setitem__(self, key, value):
+        """
+        Set the value of the given keyword in this environment
+        """
+        if not isinstance(key, str):
+            raise EvaluationError('Invalid variable name: {}'.format(key))
+        self.defs[key] = value
+
+    def __getitem__(self, key):
+        """
+        Return the value of the given keyword in this environment or,
+        if not found, in its parent.
+        Will raise an EvaluationError if that keyword is not present
+        """
+        if (key in self.defs.keys()):
+            return self.defs[key]
+        if (self.parent is None):
+            raise EvaluationError('Invalid keyword: {}'.format(key))
+        return self.parent[key]
+
+    def update(self, defs_to_add):
+        """
+        Add all definitions from the given dictionary to the environment
+        Will overwrite existing definitions
+        """
+        assert isinstance(defs_to_add, dict), 'Cannot update from a non-dictionary'
+        for key in defs_to_add.keys():
+            self.__setitem__(key, defs_to_add[key])
 
 carlae_builtins = {
     '+': sum,
     '-': lambda args: -args[0] if len(args) == 1 else (args[0] - sum(args[1:])),
+    '*': lambda args: 1 if len(args)==0 else args[0]*carlae_builtins['*'](args[1:]),
+    '/': lambda args: 1/args[0] if len(args)==1 else \
+        args[0]/carlae_builtins['*'](args[1:]),
 }
+builtins_env = CarlaeEnvironment()
+builtins_env.update(carlae_builtins)
 
-
-def evaluate(tree):
+def evaluate(tree, env=None):
     """
     Evaluate the given syntax tree according to the rules of the carlae
     language.
@@ -140,27 +179,50 @@ def evaluate(tree):
     >>> evaluate(['+', 3, -4.5])
     -1.5
     """
+    # default variables issue
+    # those in function line are only evaluated once
+    if env is None:
+        env = CarlaeEnvironment(builtins_env)
+
     # for single inputs
     if (not isinstance(tree, list)):
         if (isinstance(tree, str)): # for symbols, find the associated value
-            if (tree in carlae_builtins.keys()):
-                return carlae_builtins[tree]
-            else: # if not found, raise an error
-                raise EvaluationError('Cannot evaluate: {}'.format(tree))
+            return env[tree]
+            # if (tree in carlae_builtins.keys()):
+            #     return carlae_builtins[tree]
+            # else: # if not found, raise an error
+            #     raise EvaluationError('Cannot evaluate: {}'.format(tree))
         else: # otherwise it should be a number
             return tree # so return directly
 
     # catch possible issues? and ensure a useful error will be raised if necessary
     if (len(tree) == 0):
         raise EvaluationError('Empty expression encountered!')
+
+    if (tree[0] == 'define'):
+        if len(tree) != 3:
+            raise EvaluationError('Define must be given one name and one expression')
+        val = evaluate(tree[2], env)
+        env[tree[1]] = val
+        return val
     
     # if multiple values are present in this tree
-    func = evaluate(tree[0])
+    func = evaluate(tree[0], env)
     if (not callable(func)):
         raise EvaluationError('Function expected: {}'.format(func))
-    args = [evaluate(arg) for arg in tree[1:]]
+    args = [evaluate(arg, env) for arg in tree[1:]]
     return func(args)
 
+def result_and_env(tree, env=None):
+    """
+    Test function: wrapper for evaluate that also guarantees returning the environment
+    """
+    # default variables
+    if env is None:
+        env = CarlaeEnvironment(builtins_env)
+
+    res = evaluate(tree, env)
+    return res, env
 
 if __name__ == '__main__':
     # code in this block will only be executed if lab.py is the main file being
@@ -169,6 +231,8 @@ if __name__ == '__main__':
     # run doctests-- comment out before submitting!
     # import doctest
     # doctest.testmod()
+
+    #print(carlae_builtins['/']([1, 2, 3, 4, 5]))
 
     # REPL
     val = input("in> ")
